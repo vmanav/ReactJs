cd3116860c5ff305e0b1d1d0c606449f30ec7f70
vmanav <zmanav.1999@gmail.com>
2021-01-05 16:03:56 +0530

    basics of material-ui

--- matrialui-tut/src/App.js
+++ matrialui-tut/src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, ButtonGroup, Checkbox, FormControlLabel, TextField } from '@material-ui/core';
-import { Save, Delete, Check, Clear, PlayCircleFilledWhite } from '@material-ui/icons';
+import { AppBar, Toolbar, IconButton, Container, Paper, Grid, Button, ButtonGroup, Checkbox, FormControlLabel, TextField } from '@material-ui/core';
+import { Save, Delete, Check, Clear, Menu } from '@material-ui/icons';
 import { useState } from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
-import { lightGreen } from '@material-ui/core/colors';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { orange, blue } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
   root: {
     background: 'lightGreen',
     border: 0,
+    marginBottom: 25,
     borderRadius: 15,
     color: 'white',
-    padding: '0 30 px'
+    padding: '15 px 30 px'
   }
 })
 
-function MyButton() {
+const myTheme = createMuiTheme({
+  palette: {
+    // primary: {
+    //   main: orange[500],
+    // },
+    // secondary: {
+    //   main: blue[500]
+    // }
+  }
+})
 
+
+function MyButton() {
   const myClass = useStyles();
   return (
     <Button
@@ -27,10 +39,8 @@ function MyButton() {
   )
 }
 
-
 function CheckBoxExample() {
   const [checked, changeChecked] = useState(false);
-
 
   return (
     <FormControlLabel
@@ -53,34 +63,70 @@ function CheckBoxExample() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <MyButton />
-        <TextField
-          variant="outlined"
-          color="secondary"
-          type="time"
-          label="samye"
-        />
-        <CheckBoxExample />
-        <ButtonGroup
-          color="primary"
-          variant="contained">
-          <Button
-            startIcon={<Save />}
-          >
-            Save
-        </Button>
-          <Button
-            startIcon={<Delete />}
-          >
-            Discard
-          </Button>
-        </ButtonGroup>
+    <ThemeProvider theme={myTheme}>
+      <Container maxWidth="sm">
+        <div className="App">
+          <header className="App-header">
+            <AppBar>
+              <Toolbar>
+                <IconButton>
+                  <Menu>
+                  </Menu>
+                </IconButton>
+                    h6 typography
+                    <Button>
+                  Login
+                    </Button>
+              </Toolbar>
+            </AppBar>
+            <MyButton />
 
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div >
+            {/* <TextField
+              variant="outlined"
+              color="secondary"
+              type="time"
+              label="samye"
+            /> */}
+            <Grid container spacing={2} justify="center">
+              <Grid item xs={12} sm={6}>
+                <Paper style={{
+                  height: 75, width: '100%'
+                }} />
+              </Grid>
+              <Grid item>
+                <Paper style={{
+                  height: 75, width: 50
+                }} />
+              </Grid>
+              <Grid item>
+                <Paper style={{
+                  height: 75, width: 50
+                }} />
+              </Grid>
+            </Grid>
+
+            <CheckBoxExample />
+
+            <ButtonGroup
+              color="primary"
+              variant="contained">
+              <Button
+                startIcon={<Save />}
+              >
+                Save
+        </Button>
+              <Button
+                startIcon={<Delete />}
+              >
+                Discard
+          </Button>
+            </ButtonGroup>
+
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div >
+      </Container>
+    </ThemeProvider>
   );
 }
 
